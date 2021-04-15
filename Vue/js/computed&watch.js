4261d7ad20ebada9956d82793db85127f0a2733a
hyunsu0224 <zsdry19203@gmail.com>
2021-04-15 18:31:21 +0900

    2021/04/15

--- Vue/js/computed&watch.js
+++ Vue/js/computed&watch.js
@@ -38,7 +38,7 @@
 
     var vm = new Vue({
         el: '#demo',
-        data: {
+    data: {
             firstName: 'Foo',
             lastName: 'Bar',
             fullName: 'Foo Bar'
@@ -55,12 +55,56 @@
 
     watch속성은 명령형 프로그래밍 방식이라고도 칭하는데, 감시할 데이터를 지정하고 그 데이터가
     변경되면 이러이러한 함수를 실행해라!! 라는 방식이고
+    입력값을 토대로 해서 다음 결과를 표기(답이 정해져있음)
 
-    computed속성은 목표를 한 데이터의 내용을 정의해주는 방식
+    computed속성은 목표를 한 데이터의 내용을 정의해주는 방식(입력값을 토대로 하지만 함수에 따라서 최종 결과가 변경이 됨)
     쉽게 말해 watch는 보여지는 결과에 따라 나오는 값이 다르지만 
     computed는 지정된 값을 정의를 통해 변경해주는 것
 */
 
 /*
-    computed 와 setter
+    computed속성의 setter함수
+
+    computed속성에는 기본적으로 getter함수만 가지고 있지만, 필요에 따라서 setter 함수를 만들어 쓸수 있다.
+
+    var vm = new Vue({
+        el : '#demo',
+        data : {
+            firstName: 'Foo',
+            lastName: 'Bar',
+            fullName: 'Foo Bar'
+        }
+        computed: {
+            fullName: {
+                // getter
+                get: function () {
+                return this.firstName + ' ' + this.lastName
+                },
+                // setter
+                set: function (newValue) {
+                var names = newValue.split(' ')
+                this.firstName = names[0]
+                this.lastName = names[names.length - 1]
+                }
+            }
+        }
+    })
+
+    이제 vm.fullName = 'John Doe'를 실행하면 설정자가 호출되고 vm.firstName과 vm.lastName이 그에 따라 업데이트 됩니다.
+
+*/
+
+/*
+    watch 속성
+    대부분의 경우 computed 속성이 더 적합하지만 사용자가 만든 감시자가 필요한 경우가 있습니다. 
+    그래서 Vue는 watch 옵션을 통해 데이터 변경에 반응하는 보다 일반적인 방법을 제공합니다. 
+    이는 데이터 변경에 대한 응답으로 비동기식 또는 시간이 많이 소요되는 조작을 수행하려는 경우에 가장 유용합니다.
+
+    <div id="watch-example">
+        <p>
+            yes/no give question :
+            <input v-model="question">
+        </p>
+        <p>{{ answer }}</p>
+    </div>
 */
