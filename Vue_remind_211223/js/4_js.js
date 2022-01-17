ca29baea8d64206e2e30275bec947682c71e923a
hyunsu0224 <zsdry19203@gmail.com>
2022-01-17 18:27:41 +0900

    20220117_study_2

--- Vue_remind_211223/js/4_js.js
+++ Vue_remind_211223/js/4_js.js
@@ -1,16 +1,45 @@
-/* second-title */
-var app2 = new Vue({
-    el : "#app-2",
+/* first-title */
+
+var vm = new Vue({
+    el : "#example",
     data : {
-        rawHtml : '<span style="color : red">This should be red.</span>'
+        message : "안녕하세요"
+    },
+    computed : {
+        //계산된 getter
+        reversedMessage : function(){
+            // 'this'는 vm 인스턴스를 가리킨다.
+            return this.message.split('').reverse().join('');
+        }
     }
 })
 
-
-/* third-title */
-var app3 = new Vue({
-    el : "#app-3",
+/* first_2-title */
+var vm2 = new Vue({
+    el : "#example2",
     data : {
-        seen : true
+        message : "HELLO"
+    },
+    methods: {
+        reversedMessage: function () {
+          return this.message.split('').reverse().join('')
+        }
+      }
+})
+
+/* first_3-title */
+var data = { a : 1}
+var vm3 = new Vue({
+    el : "#example3",
+    data : {
+        message : "dataNow function"
+    },
+    computed : {
+        now : function(){
+            return Data.now()
+        }
     }
 })
+
+vm3.a === data.a
+
