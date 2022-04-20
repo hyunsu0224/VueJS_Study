5ea66582c9582f556c3809177dee221b657d4468
hyunsu0224 <zsdry19203@gmail.com>
2022-04-20 14:23:02 +0900

    220420_study

--- Vue_remind_220414/js/index1.js
+++ Vue_remind_220414/js/index1.js
@@ -39,61 +39,83 @@ const count_number = {
     mounted() {
         setInterval(() => {
             this.counter++
-        }, 10000)
-    },
-    methods : {
-        clearInterval()
+        }, 5000)
     }
 }
 Vue.createApp(count_number).mount('#count_num');
 
 
-new Vue({
-    el: '#app',
-    // =======================
-    data: {
-      title: 'Timer',
-      timer: null,
-      totalTime: (1 * 60),
-      resetButton: false,
-    },
-    // =======================
-    methods: {
-      startTimer: function() {
-        this.timer = setInterval(() => this.countdown(), 1000);
-        this.resetButton = true;
-      },
-      stopTimer: function() {
-        clearInterval(this.timer);
-        this.timer = nullthis.resetButton = true;
-      },
-      resetTimer: function() {
-        this.totalTime = (1 * 60);
-        clearInterval(this.timer);
-        this.timer = null;
-        this.resetButton = false;
-      },
-      padTime: function(time) {
-        return (time < 10 ? '0' : '') + time;
-      },
-      countdown: function() {
-        if(this.totalTime >= 1) {
-          this.totalTime--;
-        } else {
-          this.totalTime = 0;
-          this.resetTimer;
+const EventHandling = {
+    data() {
+        return {
+            message : 'Hello Vue.js!'
         }
-      }
     },
-    // =======================
-    computed: {
-      minutes: function() {
-        const minutes = Math.floor(this.totalTime / 60);
-        return this.padTime(minutes);
-      },
-      seconds: function() {
-        const seconds = this.totalTime - (this.minutes * 60);
-        return this.padTime(seconds);
-      }
+    methods : {
+        reverseMessage() {
+            this.message = this.message
+                .split('')
+                .reverse()
+                .join('')
+        }
     }
-  });
+}
+Vue.createApp(EventHandling).mount('#event-handling');
+
+const ConditionalRendering = {
+    data() {
+        return {
+            go : true
+        }
+    }
+}
+Vue.createApp(ConditionalRendering).mount("#conditional-rendering");
+
+const ListRendering = {
+    data() {
+        return {
+            todos : [
+                { text : 'Learn JS'},
+                { text : 'Learn Vue'},
+                { text : 'Build something awesome'}
+            ]
+        }
+    }
+}
+Vue.createApp(ListRendering).mount('#list-rendering');
+
+
+const TodoList = {
+   data() {
+       return {
+           groceryList : [
+               { id : 0, text:'a'},
+               { id : 1, text:'b'},
+               { id : 2, text:'cdefg'}
+           ]
+       }
+   } 
+}
+const app = Vue.createApp(TodoList);
+
+app.component('todo-item', {
+    props : ['todo'],
+    template : `<li>{{todo.text}}</li>`
+})
+
+app.mount('#todo-list-app')
+
+
+// const app = Vue.createApp({
+//     /* options */
+// })
+
+const app_1 = Vue.createApp({})
+app_1.component('SearchInput', SearchInputComponent)
+app_1.directive('focus', FocusDirective)
+app_1.use(LocalPlugin)
+
+Vue.createApp({})
+   .component('SearchInput', SearchInputComponent)
+   .directive('focus', FocusDirective)
+   .use(LocalePlugin)
